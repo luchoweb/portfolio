0ca216fa0f45877cb4c3dc8b83af11c79800c286
Lucho Web <luchoweb.dev@gmail.com>
2023-01-23 10:59:53 -0500

    Menu added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,18 @@
+import Menu from './menu';
+
 import './styles.scss'
 
 function Header() {
   return (
-    <header className="pt-3 pb-3 mb-0 mb-md-3">
+    <header className="pt-3 pb-3 mb-3">
         <div className='container'>
           <div className="row">
-            <div className="col-6">
-              <p className='title'><strong>Lucho Web</strong></p>
+            <div className="col-12 text-center text-md-start col-md-3 mb-2 mb-md-0">
+              <p className='title m-0'><strong>Lucho Web</strong></p>
             </div>
-            <div className="col-6"></div>
+            <div className="col-12 col-md-9">
+              <Menu />
+            </div>
           </div>
         </div>
       </header>
---
+++ src/components/Header/menu.jsx
@@ -0,0 +1,28 @@
+function Menu() {
+  return (
+    <ul className="menu list-unstyled m-0 p-0 d-flex align-items-center gap-4 justify-content-center justify-content-md-end">
+      <li className="menu__item">
+        <a href="#" className="menu__link">
+          Home
+        </a>
+      </li>
+      <li className="menu__item">
+        <a href="#" className="menu__link">
+          Dev Stacks
+        </a>
+      </li>
+      <li className="menu__item">
+        <a href="#" className="menu__link">
+          Soft Skills
+        </a>
+      </li>
+      <li className="menu__item">
+        <a href="#" className="menu__link">
+          Get in touch!
+        </a>
+      </li>
+    </ul>
+  )
+}
+
+export default Menu
